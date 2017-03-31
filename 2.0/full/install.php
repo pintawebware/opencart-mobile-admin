6499feabff3b1058a9676e7ffbb10b675dee5740
ruslan <ruslan@MacBook-Pro-ruslan.local>
2017-03-31 18:12:25 +0300

    remove old build module

--- 2.0/full/install.php
+++ 2.0/full/install.php
@@ -1,14 +1,20 @@
 <?php 
 
-$this->db->query("CREATE TABLE IF NOT EXISTS " . DB_PREFIX."user_token_mob_api (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id INT NOT NULL, token VARCHAR(32) NOT NULL )");
-$this->db->query("CREATE TABLE IF NOT EXISTS " . DB_PREFIX."user_device_mob_api (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id INT NOT NULL, device_token VARCHAR(500) , os_type VARCHAR(20))");
+$this->db->query("CREATE TABLE IF NOT EXISTS " . DB_PREFIX."user_token_mob_api (id INT NOT NULL PRIMARY KEY, user_id INT NOT NULL, token VARCHAR(32) NOT NULL )");
+$this->db->query("CREATE TABLE IF NOT EXISTS " . DB_PREFIX."user_device_mob_api (id INT NOT NULL PRIMARY KEY, user_id INT NOT NULL, device_token VARCHAR(500) , os_type VARCHAR(50))");
 
 $this->load->model('setting/setting');
 
 	$this->model_setting_setting->editSetting('apimodule', ['apimodule_status'=>1,'version'=>1.7]);
-if(VERSION == '2.0.0.0'){
+if(VERSION == '2.0.0.0' || VERSION == '2.0.0.0a1'){
     $this->load->model('tool/event');
 	$this->model_tool_event->addEvent('apimodule', 'post.order.history.add', 'module/apimodule/sendNotifications');
+}elseif(VERSION == '2.0.0.0a1'){
+	$this->load->model('tool/event');
+	$this->model_tool_event->setHandler('apimodule', 'post.order.history.add', 'module/apimodule/sendNotifications');
+}elseif(VERSION == '2.2.0.0'){
+	$this->load->model('tool/event');
+	$this->model_tool_event->addEvent('apimodule', 'catalog/model/checkout/order/addOrderHistory/after', 'module/apimodule/sendNotifications');
 }else{
 	$this->load->model('extension/event');
 	$this->model_extension_event->addEvent('apimodule', 'post.order.history.add', 'module/apimodule/sendNotifications');
--- install.php
+++ install.php
@@ -6,9 +6,12 @@ $this->db->query("CREATE TABLE IF NOT EXISTS " . DB_PREFIX."user_device_mob_api 
 $this->load->model('setting/setting');
 
 	$this->model_setting_setting->editSetting('apimodule', ['apimodule_status'=>1,'version'=>1.7]);
-if(VERSION == '2.0.0.0'){
+if(VERSION == '2.0.0.0' || VERSION == '2.0.0.0a1'){
     $this->load->model('tool/event');
 	$this->model_tool_event->addEvent('apimodule', 'post.order.history.add', 'module/apimodule/sendNotifications');
+}elseif(VERSION == '2.0.0.0a1'){
+	$this->load->model('tool/event');
+	$this->model_tool_event->setHandler('apimodule', 'post.order.history.add', 'module/apimodule/sendNotifications');
 }else{
 	$this->load->model('extension/event');
 	$this->model_extension_event->addEvent('apimodule', 'post.order.history.add', 'module/apimodule/sendNotifications');
