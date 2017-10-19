698f5ce6fe8e1af519c0cadf9758431fd4024517
Yarik Haidai <y.haidai@pinta.com.ua>
2017-10-19 13:46:15 +0300

    returned the price for the current currency for 1.5 version

--- 1.5/catalog/controller/module/apimodule.php
+++ 1.5/catalog/controller/module/apimodule.php
@@ -661,7 +661,8 @@ class ControllerModuleApimodule extends Controller
                         $product['quantity'] = number_format($products[$i]['quantity'], 2, '.', '');
                     }
                     if (isset($products[$i]['price']) && $products[$i]['price'] != '') {
-                        $product['price'] = number_format($products[$i]['price'], 2, '.', '');
+                        //$product['price'] = number_format($products[$i]['price'], 2, '.', '');
+                        $product['price'] = number_format($this->tax->calculate($products[$i]['price'], $products[$i]['tax_class_id'], $this->config->get('config_tax')), 2, '.', '');
                     }
                     $product['product_id'] = $products[$i]['product_id'];
 
@@ -1818,7 +1819,8 @@ class ControllerModuleApimodule extends Controller
             } else {
                 $data['image'] = '';
             }
-            $data['price'] = number_format($product['price'], 2, '.', '');
+            //$data['price'] = number_format($product['price'], 2, '.', '');
+            $data['price'] = number_format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')), 2, '.', '');
             $data['name'] = strip_tags(htmlspecialchars_decode($product['name']));
             $data['currency_code'] = $this->model_module_apimodule->getDefaultCurrency();
             $product_categories = $this->model_module_apimodule->getProductCategoriesMain($product['product_id']);
@@ -1935,7 +1937,8 @@ class ControllerModuleApimodule extends Controller
                     $currency = $this->model_module_apimodule->getDefaultCurrency();
                 }
                 $response['currency_code'] = $currency;
-                $response['price'] = $this->calculatePrice($product['price'], $currency);
+                //$response['price'] = $this->calculatePrice($product['price'], $currency);
+                $response['price'] = number_format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')), 2, '.', '');
                 $this->load->model('tool/image');
                 $product_img = $this->model_module_apimodule->getProductImages($id);
                 $response['images'] = [];
